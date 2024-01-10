5edaca2709e7190ece12ea95362713f0908b1ae3
Himanshu <phimanshu925@gmail.com>
2024-01-10 23:58:56 +0530

    Student.cs and DBManager.cs

---
+++ BOL/Student.cs
@@ -0,0 +1,14 @@
+namespace BOL;
+
+public class Student{
+
+    public int Prn{get;set;}
+    public int RollNo{get;set;}
+    public string FirstName{get;set;}
+    public string LastName{get;set;}
+    public string CourseId{get;set;}
+    public string Email{get;set;}
+    public string AdharNo{get;set;}
+    public string Address{get;set;}
+
+}
---
+++ DAL/DBManager.cs
@@ -0,0 +1,14 @@
+using BOL;
+using MySql.Data.MySqlClient;
+public class DBManeger{
+   
+       public class DBManeger(){}
+
+       public List<Student> GetAll(){
+
+        List<Student> students = new List<Student>();
+
+        MySqlConnection con = new MySqlConnection();
+
+       }
+}
